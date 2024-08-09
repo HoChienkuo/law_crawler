cff889de2ab28d60b6a005f5161be7ae6d39b7ec
he <1195745198@qq.com>
2024-08-10 01:58:40 +0800

    下载功能

--- init.py
+++ init.py
@@ -1,6 +1,46 @@
 import sqlite3
 
-from sql import *
+# 创建info表
+sql_create_info_table = ("CREATE TABLE IF NOT EXISTS info("
+                         "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
+                         "key VARCHAR(32) NOT NULL,"
+                         "value VARCHAR(64) NOT NULL"
+                         ")")
+
+table_form = ("id VARCHAR(64) PRIMARY KEY NOT NULL,"
+              "title VARCHAR(64) NOT NULL,"
+              "url VARCHAR(128) NOT NULL,"
+              "office VARCHAR(64) NOT NULL,"
+              "type VARCHAR(32) NOT NULL,"
+              "status TINYINT NOT NULL,"
+              "publish VARCHAR(32) NOT NULL,"
+              "expiry VARCHAR(32) NOT NULL,"
+              "saved TINYINT default 0"
+              "")
+# 创建宪法表
+sql_create_xf_table = ("CREATE TABLE IF NOT EXISTS xf("
+                       f"{table_form}"
+                       ")")
+# 创建法律法规表
+sql_create_flfg_table = ("CREATE TABLE IF NOT EXISTS flfg("
+                         f"{table_form}"
+                         ")")
+# 创建行政法规表
+sql_create_xzfg_table = ("CREATE TABLE IF NOT EXISTS xzfg("
+                         f"{table_form}"
+                         ")")
+# 创建监察法规表
+sql_create_jcfg_table = ("CREATE TABLE IF NOT EXISTS jcfg("
+                         f"{table_form}"
+                         ")")
+# 创建司法解释表
+sql_create_sfjs_table = ("CREATE TABLE IF NOT EXISTS sfjs("
+                         f"{table_form}"
+                         ")")
+# 创建地方性法规表
+sql_create_dfxfg_table = ("CREATE TABLE IF NOT EXISTS dfxfg("
+                          f"{table_form}"
+                          ")")
 
 
 def database_init():
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import getopt
 import json
+import os
 import sqlite3
 import sys
 import time
@@ -12,18 +13,18 @@ def usage():
     print("python main.py --help\\--type")
     print("")
     print("Options:")
-    print("-h, --help\t\t展示帮助并退出函数")
-    print("-t, --type\t\t选择要爬取的法律类型,使用方法 python main.py --type 0")
-    print("\t\t\t\t0: 以下全部")
-    print("\t\t\t\t1: xf(宪法)")
-    print("\t\t\t\t2: flfg(法律法规)")
-    print("\t\t\t\t3: xzfg(行政法规)")
-    print("\t\t\t\t4: jcfg(监察法规)")
-    print("\t\t\t\t5: sfjs(司法解释)")
-    print("\t\t\t\t6: dfxfg(地方性法规)")
-    print("--download\t\t下载到项目download文件夹")
-    print("--begin\t\t爬取的开始页，默认第一页")
-    print("--end\t\t爬取的结束页，默认最后一页")
+    print("-h, --help\t\t\t展示帮助并退出函数")
+    print("-t, --type\t\t\t选择要爬取的法律类型,使用方法 python main.py --type 0")
+    print("\t\t\t\t\t0: 以下全部")
+    print("\t\t\t\t\t1: xf(宪法)")
+    print("\t\t\t\t\t2: flfg(法律法规)")
+    print("\t\t\t\t\t3: xzfg(行政法规)")
+    print("\t\t\t\t\t4: jcfg(监察法规)")
+    print("\t\t\t\t\t5: sfjs(司法解释)")
+    print("\t\t\t\t\t6: dfxfg(地方性法规)")
+    print("-d,  --download\t\t下载到项目download文件夹")
+    print("--begin\t\t\t\t爬取的开始页，默认第一页")
+    print("--end\t\t\t\t爬取的结束页，默认最后一页")
 
 
 def get_type_cn_prefix(type_num):
@@ -117,6 +118,68 @@ def transfer_data_list(data_list):
     return res
 
 
+def get_document_url(legal_id):
+    base_url = f'https://wb.flk.npc.gov.cn'
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Connection": "keep-alive",
+        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"
+    }
+    body = {
+        "id": legal_id
+    }
+    count = 1
+    res = {}
+    while count <= 3:
+        try:
+            response = requests.post(f"{base_url}/api/detail", data=body, headers=headers)
+            response.raise_for_status()
+            response.encoding = 'utf-8'
+            res = json.loads(response.text)
+            break
+        except Exception as e:
+            print(f"请求接口{base_url}/api/detail错误，第{count}次请求，将重试三次")
+            print("错误详情: ", str(e))
+            count += 1
+            time.sleep(1)
+    if count == 3:
+        raise ConnectionError(f"连接{base_url}/api/detail失败，有可能是IP被封，请更新IP尝试")
+    doc_name = res['result']['body'][0]['path']
+    return f'{base_url}{doc_name}'
+
+
+def download_source(type_num):
+    table_name = get_type_cn_prefix(type_num)
+    if not os.path.isdir(f'download/{table_name}'):
+        os.mkdir(f'download/{table_name}')
+    connect = sqlite3.connect('data/database.db')
+    cursor = connect.cursor()
+    sql = f'SELECT id, title, saved FROM {table_name}'
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    for row in rows:
+        legal_id, legal_title, saved = row
+        update_sql = f'UPDATE {table_name} SET saved = 1 WHERE id = {legal_id}'
+        if saved == 1:
+            continue
+        if os.path.exists(f'download/{table_name}/{legal_title}.*'):
+            cursor.execute(update_sql)
+            connect.commit()
+            print(f"file {legal_title} exist, save result to database success")
+            continue
+        doc_url = get_document_url(legal_id)
+        file_extension = os.path.splitext(doc_url)[1]
+        response = requests.get(doc_url)
+        with open(f'download/{table_name}/{legal_title}.{file_extension}', 'wb') as f:
+            f.write(response.content)
+        cursor.execute(update_sql)
+        connect.commit()
+        print(f"file {legal_title} saved, save result to database success")
+
+
 def law_crawler(type_num: int, download_flag: bool, begin_page: int, end_page: int):
     if type_num == 0:  # all
         crawl_all(download_flag, begin_page, end_page)
@@ -143,13 +206,15 @@ def law_crawler(type_num: int, download_flag: bool, begin_page: int, end_page: i
     connect = sqlite3.connect('data/database.db')
     cursor = connect.cursor()
     # 已存在的项忽略
-    sql = f'INSERT OR IGNORE INTO {get_type_cn_prefix(type_num)} VALUES (?, ?, ?, ?, ?, ?, ?, ?)'
+    sql = f'INSERT OR IGNORE INTO {get_type_cn_prefix(type_num)} VALUES (?, ?, ?, ?, ?, ?, ?, ?, 0)'
     # 多次插入
     cursor.executemany(sql, new_data_list)
     connect.commit()
     cursor.close()
     connect.close()
     print("save result to database success")
+    if download_flag:
+        download_source(type_num)
 
 
 if __name__ == '__main__':
@@ -157,14 +222,14 @@ if __name__ == '__main__':
     crawl_type = -1
     begin = -1
     end = -1
-    opts, args = getopt.getopt(sys.argv[1:], "ht:", ["help", "type=", "download", "begin=", "end="])
+    opts, args = getopt.getopt(sys.argv[1:], "ht:d", ["help", "type=", "download", "begin=", "end="])
     if len(opts) == 0:
         raise ValueError("参数错误，使用-h或--help查看帮助")
     for opt_name, opt_value in opts:
         if opt_name in ("-h", "--help"):
             usage()
             exit(0)
-        if opt_name in "--download":
+        if opt_name in ("-d", "--download"):
             download = True
             continue
         if opt_name in ("-t", "--type"):
